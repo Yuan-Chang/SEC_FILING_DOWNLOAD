4a700368ec9cd3407bf053a051043c85a9b1f59b
Yuan Chang Chang <yuan@yuans-mbp.lan>
2024-04-15 11:31:49 -0700

    remove testing files

--- download_sec_filing_report.py
+++ download_sec_filing_report.py
@@ -1,12 +1,17 @@
 from sec_edgar_api import EdgarClient
 import json
 from datetime import datetime
-from utils.util import filter_frame, get_quarter_values, sorted_by_property, get_sorted_year_report, \
-    merge_quarter_result, quarter_type_operatingIncomeLoss, quarter_type_incomeBeforeIncomeTax, \
-    quarter_type_incomeBeforeIncomeTaxAndEquityInterest, quarter_type_incomeTaxExpenseBenefit
+from utils.util import filter_frame, sorted_by_property, merge_property_value, get_simple_quarter, toMillion, \
+    QuarterSimple
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter, column_index_from_string
+
+import utils.Utils as utils
+from openpyxl import Workbook, worksheet
+from openpyxl.styles import PatternFill, Alignment, Font, Color
 
 
-def get_report_summary(cik: str = "723125", number_of_quarter: int = 10):
+def get_report_summary(wb: Workbook, cik: str, company_name: str):
     edgar = EdgarClient("<Sample Company Name> <Admin Contact>@<Sample Company Domain>")
 
     # 723125
@@ -14,146 +19,108 @@ def get_report_summary(cik: str = "723125", number_of_quarter: int = 10):
 
     result = edgar.get_company_facts(cik=cik)
 
-    operatingIncomeLoss = result['facts']['us-gaap']['OperatingIncomeLoss']
     operatingIncomeLossList = result['facts']['us-gaap']['OperatingIncomeLoss']["units"]["USD"]
-    taxRate = result['facts']['us-gaap']['EffectiveIncomeTaxRateContinuingOperations']
-    taxRateList = result['facts']['us-gaap']['EffectiveIncomeTaxRateContinuingOperations']["units"]["pure"]
-    incomeTaxExpenseBenefit = result['facts']['us-gaap']['IncomeTaxExpenseBenefit']
     incomeTaxExpenseBenefitList = result['facts']['us-gaap']['IncomeTaxExpenseBenefit']["units"]["USD"]
 
-    incomeBeforeIncomeTaxList = []
+    incomeBeforeIncomeTaxList = None
     if 'IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest' in result['facts'][
         'us-gaap']:
-        incomeBeforeIncomeTax = result['facts']['us-gaap'][
-            'IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest']
         incomeBeforeIncomeTaxList = result['facts']['us-gaap'][
             'IncomeLossFromContinuingOperationsBeforeIncomeTaxesExtraordinaryItemsNoncontrollingInterest']["units"][
             "USD"]
 
-    incomeBeforeIncomeTaxAndEquityInterestList = []
+    incomeBeforeIncomeTaxAndEquityInterestList = None
     if 'IncomeLossFromContinuingOperationsBeforeIncomeTaxesMinorityInterestAndIncomeLossFromEquityMethodInvestments' in \
             result['facts']['us-gaap']:
-        incomeBeforeIncomeTaxAndEquityInterest = result['facts']['us-gaap'][
-            'IncomeLossFromContinuingOperationsBeforeIncomeTaxesMinorityInterestAndIncomeLossFromEquityMethodInvestments']
         incomeBeforeIncomeTaxAndEquityInterestList = result['facts']['us-gaap'][
             'IncomeLossFromContinuingOperationsBeforeIncomeTaxesMinorityInterestAndIncomeLossFromEquityMethodInvestments'][
             "units"]["USD"]
 
     # sort the list
     operatingIncomeLossList = sorted_by_property(operatingIncomeLossList, "end")
-    taxRateList = sorted_by_property(taxRateList, "end")
-    incomeTaxExpenseBenefitList = sorted_by_property(incomeTaxExpenseBenefitList, "end")
-    incomeBeforeIncomeTaxList = sorted_by_property(incomeBeforeIncomeTaxList, "end")
-    incomeBeforeIncomeTaxAndEquityInterestList = sorted_by_property(incomeBeforeIncomeTaxAndEquityInterestList, "end")
 
     # filter data with "frame" property
     operatingIncomeLossList = filter_frame(operatingIncomeLossList)
-    taxRateList = filter_frame(taxRateList)
-    incomeTaxExpenseBenefitList = filter_frame(incomeTaxExpenseBenefitList)
-    incomeBeforeIncomeTaxList = filter_frame(incomeBeforeIncomeTaxList)
-    incomeBeforeIncomeTaxAndEquityInterestList = filter_frame(incomeBeforeIncomeTaxAndEquityInterestList)
 
-    operatingIncomeLossYearReport = get_quarter_values(operatingIncomeLossList)
-    incomeTaxExpenseBenefitYearReport = get_quarter_values(incomeTaxExpenseBenefitList)
-    incomeBeforeIncomeTaxYearReport = get_quarter_values(incomeBeforeIncomeTaxList)
-    incomeBeforeIncomeTaxAndEquityInterestYearReport = get_quarter_values(incomeBeforeIncomeTaxAndEquityInterestList)
+    row_names = [
+        "Fiscal Quarter",
+        "Calendar Quarter",
+        "start",
+        "end",
+        "Operating Income Loss",
+        "Income Tax Expense Benefit",
+        "Income Before Income Tax",
+        "Income Before Income Tax And EquityInterest"]
 
-    merge_quarter_result(toReport=operatingIncomeLossYearReport, fromReport=operatingIncomeLossYearReport,
-                         type=quarter_type_operatingIncomeLoss)
-    merge_quarter_result(toReport=operatingIncomeLossYearReport, fromReport=incomeTaxExpenseBenefitYearReport,
-                         type=quarter_type_incomeTaxExpenseBenefit)
-    merge_quarter_result(toReport=operatingIncomeLossYearReport, fromReport=incomeBeforeIncomeTaxYearReport,
-                         type=quarter_type_incomeBeforeIncomeTax)
-    merge_quarter_result(toReport=operatingIncomeLossYearReport,
-                         fromReport=incomeBeforeIncomeTaxAndEquityInterestYearReport,
-                         type=quarter_type_incomeBeforeIncomeTaxAndEquityInterest)
+    # consolidate property
+    merge_property_value(toList=operatingIncomeLossList, fromList=operatingIncomeLossList,
+                         name=row_names[4])
+    merge_property_value(toList=operatingIncomeLossList, fromList=incomeTaxExpenseBenefitList,
+                         name=row_names[5])
+    merge_property_value(toList=operatingIncomeLossList, fromList=incomeBeforeIncomeTaxList,
+                         name=row_names[6])
+    merge_property_value(toList=operatingIncomeLossList, fromList=incomeBeforeIncomeTaxAndEquityInterestList,
+                         name=row_names[7])
 
-    res_list = []
-    for key in operatingIncomeLossYearReport.keys():
-        res_list.append(operatingIncomeLossYearReport[key])
-    res_list.sort(key=lambda x: x.year, reverse=True)
-
-    quarter_number = number_of_quarter
-    count = 0
-    for yearReport in res_list:
-        quarter_list = yearReport.getQuartersInOrder()
-
-        for quarter in quarter_list:
-            count = count + 1
-
-            # print
-            print(f"{quarter.cy_quarter_name}({quarter.start} to {quarter.end})")
-            print(f"---------------------------")
-            print(f"operatingIncomeLoss: {quarter.operatingIncomeLoss}")
-            print(f"incomeTaxExpenseBenefit: {quarter.incomeTaxExpenseBenefit}")
-            print(f"incomeBeforeIncomeTax: {quarter.incomeBeforeIncomeTax}")
-            print(f"incomeBeforeIncomeTaxAndEquityInterest: {quarter.incomeBeforeIncomeTaxAndEquityInterest}")
-            print(f"\n")
-
-            if count == quarter_number:
-                break
-
-        if count == quarter_number:
-            break
+    # operatingIncomeLossList.reverse()
+    # res_list = []
+    #
+    # for qDict in operatingIncomeLossList:
+    #     print(qDict)
+    #     q = get_simple_quarter(qDict)
+    #
+    #     if q.form == "10-Q":
+    #         res_list.insert(0, qDict)
+    #     elif q.form == "10-K":
+    #         last_q = {}
 
 
-company_dict = {"MU": "723125"}
+    sheet = wb.create_sheet(title=company_name)
+
+    # write the row
+    for row_index, row_name in enumerate(row_names):
+        cell = sheet.cell(row=row_index + 1, column=1)
+        cell.value = row_name
+        cell.alignment = Alignment(horizontal='left', vertical='center', wrap_text=True)
+        cell.font = Font(size="12", bold=True)
+        # cell.fill = PatternFill(start_color='FFCCE5FF',
+        #                         end_color='FFCCE5FF',
+        #                         fill_type='solid')
+        sheet.row_dimensions[row_index + 1].height = 40
+    sheet.column_dimensions[get_column_letter(1)].width = 40
+
+    for column_index, qDict in enumerate(operatingIncomeLossList):
+        sheet.column_dimensions[get_column_letter(column_index + 2)].width = 15
+        q = get_simple_quarter(qDict=qDict)
+        values = [q.fp, q.frame, q.start, q.end, q.operatingIncomeLoss, q.incomeTaxExpenseBenefit,
+                  q.incomeBeforeIncomeTax, q.incomeBeforeIncomeTaxAndEquityInterest]
+
+        for row_index, row_name in enumerate(row_names):
+            cell = sheet.cell(row=row_index + 1, column=column_index + 2)
+            cell.value = values[row_index]
+            cell.alignment = Alignment(horizontal='left', vertical='center', wrap_text=True)
+            cell.font = Font(size="12", color=Color())
+            # cell.fill = PatternFill(start_color='FF99CCFF',
+            #                         end_color='FF99CCFF',
+            #                         fill_type='solid')
+
+
+output_folder = "output/"
+result_file = output_folder + "result.xlsx"
+utils.delete_directory(output_folder)
+utils.create_directory(output_folder)
+
+company_dict = {"MU": "723125", "AVGO": "1730168", "ADI": "6281", "TXN": "97476", "MRVL": "1835632", "SNPS": "883241",
+                "CDNS": "813672", "DELL": "1571996", "AAPL": "320193", "HP": "46765"}
+
+# company_dict = {"MU": "723125"}
+
+wb = Workbook()
 
 for name in company_dict:
     cik = company_dict[name]
-    print(f"Name:{name} cik:{cik}")
-    print(f"=======================================")
-    get_report_summary(cik=cik, number_of_quarter=12)
 
-# for year in operatingIncomeLossYearReport.keys():
-#     incomeTaxExpenseBenefitYear = incomeTaxExpenseBenefitYearReport[year]
-#     q1 = incomeTaxExpenseBenefitYear.q1
-#     q2 = incomeTaxExpenseBenefitYear.q2
-#     q3 = incomeTaxExpenseBenefitYear.getQ3()
-#     q4 = incomeTaxExpenseBenefitYear.q4
-#
-#     if year in incomeBeforeIncomeTaxYearReport:
-#         incomeBeforeIncomeTaxYearReportYear = incomeBeforeIncomeTaxYearReport[year]
-#
-#         if (q1 is not None) and (incomeBeforeIncomeTaxYearReportYear.q1 is not None):
-#             q1.incomeBeforeIncomeTax = incomeBeforeIncomeTaxYearReportYear.q1.incomeBeforeIncomeTax
-#
-#         if (q2 is not None) and (incomeBeforeIncomeTaxYearReportYear.q2 is not None):
-#             q2.incomeBeforeIncomeTax = incomeBeforeIncomeTaxYearReportYear.q2.incomeBeforeIncomeTax
-#
-#         if (q3 is not None) and (incomeBeforeIncomeTaxYearReportYear.q3 is not None):
-#             q3.incomeBeforeIncomeTax = incomeBeforeIncomeTaxYearReportYear.q3.incomeBeforeIncomeTax
-#
-#         if (q4 is not None) and (incomeBeforeIncomeTaxYearReportYear.q4 is not None):
-#             q4.incomeBeforeIncomeTax = incomeBeforeIncomeTaxYearReportYear.q4.incomeBeforeIncomeTax
-#
-#
-#     if year in incomeBeforeIncomeTaxAndEquityInterestYearReport:
-#         incomeBeforeIncomeTaxAndEquityInterestYear = incomeBeforeIncomeTaxAndEquityInterestYearReport[year]
+    get_report_summary(wb=wb, cik=cik, company_name=name)
 
-
-# Sort the result
-# sorted_operatingIncomeLossYearReport = get_sorted_year_report(operatingIncomeLossYearReport)
-# sorted_incomeTaxExpenseBenefitYearReport = get_sorted_year_report(incomeTaxExpenseBenefitYearReport)
-# sorted_incomeBeforeIncomeTaxYearReport = get_sorted_year_report(incomeBeforeIncomeTaxYearReport)
-# sorted_incomeBeforeIncomeTaxAndEquityInterestYearReport = get_sorted_year_report(
-#     incomeBeforeIncomeTaxAndEquityInterestYearReport)
-#
-# for index, report in enumerate(sorted_operatingIncomeLossYearReport):
-
-
-# res_list = []
-# for key in incomeTaxExpenseBenefitYearReport.keys():
-#     res_list.append(incomeTaxExpenseBenefitYearReport[key])
-# res_list.sort(key=lambda x: x.year, reverse=True)
-
-# for year_report in res_list:
-#     print(year_report)
-
-# print(res)
-
-# for data in operatingIncomeLossList:
-#         print(data)
-
-# print(operatingIncomeLossList)
-# print(incomeTaxExpenseBenefitList)
+del wb["Sheet"]
+wb.save(filename=result_file)
